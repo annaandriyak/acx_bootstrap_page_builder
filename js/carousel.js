d7b587013924de1a332f6363c263646f79e6f4d1
Anna A <anna.andriyak@novactive.com>
2023-02-13 17:55:59 +0200

    test

--- js/carousel.js
+++ js/carousel.js
@@ -1,11 +1,27 @@
 /**
  * @file
  */
+
 (function ($) {
   Drupal.behaviors.owl = {
     attach: function (context, settings) {
       $('.owl-carousel', context).each(function () {
-        $(this).owlCarousel();
+        var mobile = $(this).attr('data-mobile');
+        var desktop = $(this).attr('data-desktop');
+
+        $(this).owlCarousel({
+          loop: true,
+          margin: 10,
+          nav: true,
+          responsive: {
+            0: {
+              items: mobile
+            },
+            768: {
+              items: desktop
+            }
+          }
+        });
       });
     }
   };
